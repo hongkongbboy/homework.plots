2b83c498ddfac173d069c040fa88ebf6c021eadb
hongkongbboy <hongkongbboy@gmail.com>
2018-04-18 04:06:37 -0700

    added twitter api assignements

---
+++ python-challenge/HW7/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "EWxGV6a5bDwWzjv2Ou9RicH4U"
+consumer_secret = "9C778xQ89no6kqoZEkk6xuT9ZFweRSQ8QgcanU6JbIWo96WLrW"
+access_token = "3315493962-dXJ9SwosVP0KOVKIn37hMNIilCOmlrG5cVm5G7w"
+access_token_secret = "jL5OKgVOEQD0B0GJN70pg5mTCk1kRpfEab6ell0VSu8Qd"
